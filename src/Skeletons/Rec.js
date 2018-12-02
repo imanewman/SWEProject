8c47ea62a4c702efad78509cba9492ffe9436622
Nick Ryan <nick.ryan002@gmail.com>
2018-12-02 14:37:57 -0800

    Adding skeletons

---
+++ src/Skeletons/Rec.js
@@ -0,0 +1,6 @@
+class Rec {
+	
+	constructor(location, duration, startTime, tags, draft, eventType, contactInfo, websiteLink, newFields) {
+		
+	}
+}
---
+++ src/Skeletons/User.js
@@ -0,0 +1,40 @@
+class User {
+	
+	constructor(id, email, fullName, passwordHash, preferredEvents) {
+		this.id = id;
+		this.email = email;
+		this.fullName = fullName;
+		this.passwordHash = passwordHash;
+		this.adminPrivilege = false;
+		this.preferredEvents = preferredEvents;
+	}
+
+
+	setId(id) {
+
+	}
+
+	setEmail(email) {
+
+	}
+
+	setName(name) {
+
+	}
+
+	setPassword(password) {
+
+	}
+
+	watchEvent(recEvent) {
+
+	}
+
+	isAdmin() {
+
+	}
+
+	setAdmin(adminPrivilege) {
+		
+	}
+}
