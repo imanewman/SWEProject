6e973b304baefa728f719a42d8d0b46e49fd3884
Michael Maramba <mpmaramb@calpoly.edu>
2019-02-14 13:48:28 -0800

    index

--- src/js/RestOperations/Users/users.js
+++ src/js/RestOperations/Users/users.js
@@ -1,3 +1,4 @@
+/*
 function getUserById(userId) {
 
 }
@@ -15,3 +16,35 @@ module.exports = {
     putUser,
     deleteUser
 }
+*/
+
+var Express = require('express');
+var router = Express.Router({caseSensitive: true});
+var mysql = require('mysql');
+var cnn = require('../../../Database.js')
+
+router.baseURL = '/Prss';
+
+// GET /Users
+router.get('/', function(req, res) {
+	cnn.connect();
+	
+	// handler that sends our response and releases connection
+   var handler = function(result) {
+      res.send(result);
+      // figure out how to close connection
+   };
+   
+   console.log('GET /Users');
+   //res.send("GET /Users");
+   
+   cnn.query("SELECT * FROM Users", handler);
+});
+
+// POST /Users
+router.post('/', function(req, res) {
+   console.log('POST /Users');
+   // do stuff here
+});
+
+module.exports = router;
