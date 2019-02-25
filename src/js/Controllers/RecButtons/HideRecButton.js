45589055b664b6475c0dd5be9dc30112d364182b
imanewman <tjnewman111@gmail.com>
2019-02-25 09:30:50 -0800

    navbar style tweaks, css cleanup

--- src/js/Controllers/RecButtons/HideRecButton.js
+++ src/js/Controllers/RecButtons/HideRecButton.js
@@ -13,7 +13,13 @@ class HideRecButton extends RecButton {
     }
 
     click() {
+        let $icon = $("#" + this.recId + " .hide_button i");
 
+        if ($icon.hasClass("fa-eye")) {
+            $icon.removeClass("fa-eye").addClass("fa-eye-slash");
+        } else {
+            $icon.removeClass("fa-eye-slash").addClass("fa-eye");
+        }
     }
 }
 
